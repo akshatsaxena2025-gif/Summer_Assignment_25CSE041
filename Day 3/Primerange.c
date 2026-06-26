0ede93373ddafd16e121d76c48ed9a2e209a4540
Akshat Saxena <akshat.saxena2025@glbajajgroup.org>
2026-06-26 20:30:56 +0530

    Add files via upload

---
+++ Day 3/Primerange.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int start, end;
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+    printf("Prime numbers between %d and %d are:\n", start, end);
+    for (int i = start; i <= end; i++)
+    {
+        if (i < 2)
+            continue;
+        int isPrime = 1;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime)
+            printf("%d ", i);
+    }
+    return 0;
+}
